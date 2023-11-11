2d2a4e2b56d2dc2415617fec349870444c92d7ab
Elisa Mele <elisamele08@hotmail.com>
2023-11-11 12:31:26 -0300

    Se corrigen algunos datos

--- juego.js
+++ juego.js
@@ -1,12 +1,3 @@
-const imagen1 = document.querySelector('.imagen1');
-const imagen2 = document.querySelector('.imagen2');
-const imagen3 = document.querySelector('.imagen3');
-
-const caja1 = document.querySelector('.caja1');
-const caja2 = document.querySelector('.caja2');
-const caja3 = document.querySelector('.caja3');
-
-
 function eventoDragStart(e) {
     e.dataTransfer.setData("text/plain", e.target.id);
 }
@@ -21,15 +12,11 @@ function eventoDrop(e) {
     const draggedElement = document.getElementById(data);
     const dropTarget = e.target;
 
-    // Verificar si el elemento se está soltando en una caja
     if (dropTarget.classList.contains("caja1") || dropTarget.classList.contains("caja2") || dropTarget.classList.contains("caja3")) {
-        // Ocultar el párrafo dentro del contenedor de destino
         const paragraph = dropTarget.querySelector('p');
         if (paragraph) {
             paragraph.style.display = 'none';
         }
-
-        // Mover la imagen al contenedor
         dropTarget.appendChild(draggedElement);
     }
 }
